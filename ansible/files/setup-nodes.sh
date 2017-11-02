36eb7f091569b04f48c458b1f1ac76deb0847d3f
Anthony Thomas <ahthomas@eng.ucsd.edu>
2017-11-02 12:03:17 -0700

    Adds task to deploy other software

---
+++ ansible/files/setup-nodes.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+############################
+# install java related stuff
+############################
+
+sudo apt-get -y update
+sudo apt-get -y install openjdk-8-jdk
+
+#################
+# Install OpenMPI
+#################
+
+sudo apt-get -y install openmpi-bin openmpi-doc libopenmpi-dev
+echo "export PATH=/usr/lib64/openmpi/bin:$PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib" >> ~/.bashrc
+source ~/.bashrc
+
+##################
+# Install OpenBLAS
+##################
+
+# purge the one that comes via apt-get
+sudo apt-get -y purge libopenblas-dev
+sudo apt-get -y purge libopenblas-base
+
+# get the fortran compiler
+sudo apt-get -y update
+sudo apt-get -y install gfortran
+
+cd ~/Software
+git clone https://github.com/xianyi/OpenBLAS
+cd OpenBLAS/
+make clean
+make FC=gfortran USE_OPENMP=1
+sudo make PREFIX=/usr/lib/openblas install
+echo "export BLAS_LIB_DIR=/usr/lib/openblas" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/openblas" >> ~/.bashrc
+source ~/.bashrc
+
+###########
+# Install R
+###########
+
+sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
+gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+gpg -a --export E084DAB9 | sudo apt-key add -
+sudo apt-get -y update
+sudo apt-get -y install r-base r-base-dev
+
+##############################
+# Install Python related stuff
+##############################
+
+mkdir Software
+cd Software
+sudo apt-get -y install python-dev
+sudo apt-get -y install python-wheel
+wget https://bootstrap.pypa.io/get-pip.py
+sudo -H python get-pip.py
+sudo -H pip install --upgrade pip
+sudo -H pip install --upgrade numpy 
+sudo -H pip install --upgrade scipy 
+sudo -H pip install --upgrade pandas 
+sudo -H pip install --upgrade matplotlib 
+sudo -H pip install --upgrade ipython
+sudo -H pip install --upgrade psycopg2
+sudo -H pip install --upgrade gslab_tools
+
+##############################
+# Install Bazel and TensorFlow
+##############################
+
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get -y install bazel
+
+git clone https://github.com/tensorflow/tensorflow 
+cd tensorflow
+git checkout r1.4
+printf '\n\nn\nn\nn\nn\nn\nn\nn\nn\n' | ./configure
+bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+sudo -H pip install /tmp/tensorflow_pkg/tensorflow-1.4.0-cp27-cp27mu-linux_x86_64.whl
+
+
+#######################
+# Install Scala and SBT
+#######################
+
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+sudo apt-get update
+sudo apt-get install sbt
+
+############
+# Get Eigen3
+############
+
+cd ~/Software
+wget http://bitbucket.org/eigen/eigen/get/3.3.3.tar.gz
+tar -xvf 3.3.3.tar.gz
+cd eigen-eigen-67e894c6cd8f
+echo "export EIGEN3_INCLUDE_DIR=`pwd`" >> ~/.bashrc
+
+##################
+# Get Commons Math
+##################
+
+sudo apt-get -y install maven
+
+cd ~/Software
+wget http://mirror.stjschools.org/public/apache/commons/math/source/commons-math3-3.6.1-src.tar.gz
+tar -xvf commons-math3-3.6.1-src.tar.gz
+cd commons-math3-3.6.1-src
+mvn clean install
+cd target
+echo "export COMMONS_MATH_JAR=`pwd`/commons-math3-3.6.1.jar" >> ~/.bashrc
+
+source ~/.bashrc
+
+########################################################
+# A bit of hackyness is needed to get R to use this BLAS
+########################################################
+
+cd /usr/lib/libblas
+sudo mv libblas.so _libblas.so
+sudo mv libblas.so.3 _libblas.so.3
+
+sudo ln -s /usr/lib/openblas/lib/libopenblas.so libblas.so
+sudo ln -s /usr/lib/openblas/lib/libopenblas.so libblas.so.3
+
+############################
+# Install Greenplum Database
+############################
+
+sudo apt-get -qq update
+sudo apt-get -y install python-dev cmake git-core ccache libreadline-dev \
+      bison flex zlib1g-dev openssl libssl-dev libpam-dev libcurl4-openssl-dev \
+      libbz2-dev build-essential libapr1-dev libevent-dev libffi-dev libyaml-dev \
+      libperl-dev
+
+sudo apt-get install -y python-dev wget
+wget https://bootstrap.pypa.io/get-pip.py
+sudo -H python get-pip.py
+rm get-pip.py
+
+mkdir -p ~/gpdb/build
+cd ~/gpdb/build
+git clone https://github.com/greenplum-db/gp-xerces
+cd ~/gpdb/build/gp-xerces
+mkdir build
+cd ~/gpdb/build/gp-xerces/build
+sudo ../configure --prefix=/usr/local
+sudo make
+sudo make install
+
+sudo -H pip install --upgrade lockfile paramiko setuptools epydoc
+sudo -H pip install --upgrade psutil
+sudo -H pip install --upgrade conan
+
+cd ~/gpdb/build
+git clone https://github.com/greenplum-db/gpdb
+sudo apt-get install -y libxml2-dev vim iputils-ping
+cd ~/gpdb/build/gpdb/depends
+git checkout 5.1.0
+sudo conan remote add conan-gpdb https://api.bintray.com/conan/greenplum-db/gpdb-oss
+sudo conan install --build
+cd ~/gpdb/build/gpdb
+sudo ./configure --with-perl --with-python --with-libxml --enable-mapreduce --enable-orca --prefix=/usr/local/gpdb
+sudo make
+sudo make install
+sudo apt-get install -y libboost-dev
+
+sudo chown ubuntu:ubuntu -R ~/gpdb
+
+sudo mkdir -p /data/master
+sudo mkdir -p /data1/primary
+sudo mkdir -p /data2/primary
+export me=`whoami`
+sudo chown -R $me:$me /data/master 
+sudo chown -R $me:$me /data1
+sudo chown -R $me:$me /data2
+sudo ldconfig
---
+++ scripts/install-gpdb.sh
@@ -0,0 +1,48 @@
+cd ~/gpdb
+mkdir gpconfigs
+source /usr/local/gpdb/greenplum_path.sh
+cp $GPHOME/docs/cli_help/gpconfigs/gpinitsystem_config \
+    ~/gpdb/gpconfigs/gpinitsystem_config
+cd gpconfigs
+sudo ldconfig
+
+input="declare -a DATA_DIRECTORY=(/data1/primary /data1/primary /data1/primary\
+ /data2/primary /data2/primary /data2/primary)"
+output="declare -a DATA_DIRECTORY=(/data1/primary /data1/primary\ 
+ /data1/primary /data1/primary /data2/primary /data2/primary\ 
+ /data2/primary /data2/primary)"
+sed -i s+'${input}'+'${output}'+g gpinitsystem_config
+sed -i s/MASTER_HOSTNAME=mdw/MASTER_HOSTNAME=mycluster-master/g \
+    gpinitsystem_config
+awk '/^[[:space:]]*($|#)/{next} /mycluster/{print $2;}' /etc/hosts > \
+    hostfile_gpinitsystem
+cd ../
+printf 'y\n' | gpinitsystem -c gpconfigs/gpinitsystem_config -h \
+    gpconfigs/hostfile_gpinitsystem
+createdb
+
+echo "export MASTER_DATA_DIRECTORY=/data/master/gpseg-1" >> ~/.bashrc
+source ~/.bashrc
+
+cd ~/gpdb/build
+wget 'http://apache.org/dyn/closer.cgi?filename=madlib/1.12/apache-madlib-'\
+'1.12-src.tar.gz&action=download' -O madlib.tar.gz
+tar -xvf madlib.tar.gz
+cd ~/gpdb/build/apache-madlib-1.12-src
+mkdir build
+cd build
+cmake ../
+sudo make
+
+cd ~/gpdb/build
+for name in `awk '/^[[:space:]]*($|#)/{next} /mycluster/{print $2;}' /etc/hosts`; do
+    if [ "${name}" == "mycluster-master" ]; then
+        echo "Not Copying to Master"
+        continue
+    fi
+
+    scp -r apache-madlib-1.12-src ${name}:/home/ubuntu/gpdb/build/
+done
+
+cd ~/gpdb/build/
+./src/bin/madpack -p greenplum -s madlib install
