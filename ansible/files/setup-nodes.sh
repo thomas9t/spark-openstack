b6ff9e1f06c7fd13aad0ffb354b77f9418dfde23
Anthony Thomas <thomas9t@users.noreply.github.com>
2018-02-05 13:34:08 -0800

    Update setup-nodes.sh

--- ansible/files/setup-nodes.sh
+++ ansible/files/setup-nodes.sh
@@ -4,22 +4,22 @@
 # install java related stuff
 ############################
 
+set -e
 sudo apt-get -y update
 echo debconf shared/accepted-oracle-license-v1-1 select true | \
   sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | \
   sudo debconf-set-selections
-printf '\n' | sudo apt-add-repository ppa:webupd8team/java
+yes 'n' | sudo apt-add-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get -y install oracle-java8-installer
-sudo apt-get -y install sbt
 
 if [ -f ~/SETUP_COMPLETE ]; then
     exit 0
 fi
 
-git config --global user.email "nunya@business.com"
-git config --global user.name "R Daneel Olivaw"
+#git config --global user.email "nunya@business.com"
+#git config --global user.name "R Daneel Olivaw"
 
 #################
 # Install OpenMPI
@@ -46,23 +46,48 @@ gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 sudo apt-get -y update
 sudo apt-get -y install r-base r-base-dev
-sudo apt-get -y install libopenblas-dev
+
+##################
+# Install OpenBLAS
+##################
+
+mkdir -p ~/Software
+cd ~/Software
+if [ "${COMPILE_OPENBLAS}" != "" ]; then
+    # see: https://apache.github.io/systemml/native-backend
+    git clone https://github.com/xianyi/OpenBLAS.git
+    cd OpenBLAS
+    git checkout 114fc0bae3a
+    make clean
+    export OMP_NUM_THREADS=`nproc`
+    make USE_OPENMP=1
+    sudo make install
+    echo "export LD_LIBRARY_PATH=/opt/OpenBLAS/lib:${LD_LIBRARY_PATH}" >> ~/.bashrc
+
+    # A bit of hackiness is needed to get R to use this blas
+    if [ -d /usr/lib/libblas ]; then
+        sudo mv /usr/lib/libblas /usr/lib/_libblas
+        sudo ln -s /opt/OpenBLAS/lib /usr/lib/libblas
+        sudo ln -s /opt/OpenBLAS/lib/libopenblas.so /opt/OpenBLAS/lib/libblas.so.3
+    fi
+else
+    sudo apt-get -y install libopenblas-dev
+fi
 
 ##############################
 # Install Python related stuff
 ##############################
 
-mkdir Software
-cd Software
+cd ~/Software
 sudo apt-get -y install python-dev
 sudo apt-get -y install python-wheel
 wget https://bootstrap.pypa.io/get-pip.py
 sudo -H python get-pip.py
 sudo -H pip install --upgrade pip
-sudo -H pip install --upgrade numpy 
-sudo -H pip install --upgrade scipy 
-sudo -H pip install --upgrade pandas 
-sudo -H pip install --upgrade matplotlib 
+sudo -H pip install --upgrade numpy
+sudo -H pip install --upgrade scipy
+sudo -H pip install --upgrade pandas
+sudo -H pip install --upgrade matplotlib
 sudo -H pip install --upgrade ipython
 sudo -H pip install --upgrade psycopg2
 sudo -H pip install --upgrade gslab_tools
@@ -71,17 +96,24 @@ sudo -H pip install --upgrade gslab_tools
 # Install Bazel and TensorFlow
 ##############################
 
-#echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-#curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
-#sudo apt-get update && sudo apt-get -y install bazel
+if [ "${INSTALL_TENSORFLOW}" != ""]; then
+    # Bazel and TensorFlow have problems all the time so need to get consistent version of Bazel
+    # this is very annoying
+    mkdir ~/Software/bazel
+    cd ~/Software/bazel
+    wget https://github.com/bazelbuild/bazel/releases/download/0.9.0/bazel_0.9.0-linux-x86_64.deb
+    sudo dpkg -i bazel_0.9.0-linux-x86_64.deb
+    sudo apt-get install -f
 
-#git clone https://github.com/tensorflow/tensorflow 
-#cd tensorflow
-#git checkout r1.4
-#printf '\n\nn\nn\nn\nn\nn\nn\nn\nn\n' | ./configure
-#bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package
-#bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-#sudo -H pip install /tmp/tensorflow_pkg/`ls /tmp/tensorflow_pkg`
+    cd ~/Software
+    git clone https://github.com/tensorflow/tensorflow
+    cd tensorflow
+    git checkout r1.4
+    printf '\n\nn\nn\nn\nn\nn\nn\nn\nn\n' | ./configure
+    bazel build --config=opt --incompatible_load_argument_is_label=false //tensorflow/tools/pip_package:build_pip_package
+    bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+    sudo -H pip install /tmp/tensorflow_pkg/`ls /tmp/tensorflow_pkg`
+fi
 
 #######################
 # Install Scala and SBT
@@ -119,17 +151,6 @@ echo "export COMMONS_MATH_JAR=`pwd`/commons-math3-3.6.1.jar" >> ~/.bashrc
 
 source ~/.bashrc
 
-########################################################
-# A bit of hackyness is needed to get R to use this BLAS
-########################################################
-
-cd /usr/lib/libblas
-sudo mv libblas.so _libblas.so
-sudo mv libblas.so.3 _libblas.so.3
-
-sudo ln -s /usr/lib/openblas/lib/libopenblas.so libblas.so
-sudo ln -s /usr/lib/openblas/lib/libopenblas.so libblas.so.3
-
 ############################
 # Install Greenplum Database
 ############################
@@ -139,11 +160,6 @@ sudo apt-get -y install python-dev cmake git-core ccache libreadline-dev \
       bison flex zlib1g-dev openssl libssl-dev libpam-dev libcurl4-openssl-dev \
       libbz2-dev build-essential libapr1-dev libevent-dev libffi-dev libyaml-dev \
       libperl-dev
-
-sudo apt-get install -y python-dev wget
-wget https://bootstrap.pypa.io/get-pip.py
-sudo -H python get-pip.py
-rm get-pip.py
 
 mkdir -p ~/gpdb/build
 cd ~/gpdb/build
@@ -172,16 +188,41 @@ sudo make
 sudo make install
 sudo apt-get install -y libboost-dev
 
-sudo chown ubuntu:ubuntu -R ~/gpdb
+export me=`whoami`
+sudo chown -R ${me}:${me} ~/gpdb
 
 sudo mkdir -p /data/master
 sudo mkdir -p /data1/primary
 sudo mkdir -p /data2/primary
-export me=`whoami`
-sudo chown -R $me:$me /data/master 
+sudo chown -R $me:$me /data/master
 sudo chown -R $me:$me /data1
 sudo chown -R $me:$me /data2
 sudo ldconfig
 
-echo "export BENCHMARK_PROJECT_ROOT=/home/ubuntu/benchmark" >> ~/.bashrc
+cd ~/
+echo "export BENCHMARK_PROJECT_ROOT=/home/ubuntu/SLAB" >> ~/.bashrc
 echo "COMPLETE" > ~/SETUP_COMPLETE
+
+if [ "${COMPILE_SPARK}" != "" ]; then
+    cd ~/Software
+    # we can use the version I have precompiled on Ubuntu 16.04
+    wget http://slab:eigen@souchong.ucsd.edu/spark-2.2.0.tar.gz
+    tar -xvf spark-2.2.0.tar.gz
+    sudo mv spark-2.2.0 /usr/local/spark
+    export me=`whoami`
+    sudo chown -R ${me}:${me} /usr/local/spark
+    echo "export PATH=${PATH}:/usr/local/spark/bin" >> ~/.bashrc
+    source ~/.bashrc
+
+    # same for Hadoop
+    wget http://slab:eigen@souchong.ucsd.edu/hadoop.tar.gz
+    tar -xvf hadoop.tar.gz
+    sudo mv hadoop /usr/local/hadoop
+    sudo chown -R "${me}:${me}" /usr/local/hadoop
+    echo "export PATH=${PATH}:/usr/local/hadoop/bin" >> ~/.bashrc
+    source ~/.bashrc
+
+    sudo mkdir /mnt/hdfs
+    sudo chown ${me}:${me} /mnt/hdfs
+    /usr/local/hadoop/bin/hdfs namenode format
+fi
